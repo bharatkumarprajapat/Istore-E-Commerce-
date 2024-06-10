db589e4e4bf0558ae0e6f0daeeef7dd9cd3ab43a
Your Name <ajayy9242@gmail.com>
2024-06-10 22:41:40 +0530

    e

--- Hosting/build.sh
+++ Hosting/build.sh
@@ -1,4 +1,4 @@
-cd Hostingps
+cd Hosting
 export DATABASE_URL="postgres://closewave_django_render_user:qygJHD3zdrO3RR1j5xyB4K985wXcRf5v@dpg-cpid3pect0pc73fq97og-a.oregon-postgres.render.com/closewave_django_render"
 export SECRET_KEY="4y_=RSKwuS(&[BM*VkduGrx8Z?zu>%[2pJMQko?&a_S6i&$)kC"
 cd ..
