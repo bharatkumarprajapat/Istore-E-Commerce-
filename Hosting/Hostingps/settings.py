25faa913d7d25a701b0c59f0f68ec5230c55ec5b
Your Name <ajayy9242@gmail.com>
2024-06-11 19:12:38 +0530

    host update

--- Hosting/Hostingps/settings.py
+++ Hosting/Hostingps/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://istore-e-commerce-yqah.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
