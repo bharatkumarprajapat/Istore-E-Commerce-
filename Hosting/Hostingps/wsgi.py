353ed277eaab91438406abc42a1cdd0670ae8322
Your Name <ajayy9242@gmail.com>
2024-06-11 19:00:16 +0530

    Changes

--- Hosting/Hostingps/wsgi.py
+++ Hosting/Hostingps/wsgi.py
@@ -1,7 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Ensure the settings module is set correctly
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hostingps.settings')
-
 application = get_wsgi_application()
